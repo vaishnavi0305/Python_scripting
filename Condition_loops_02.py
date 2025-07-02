931dbc16e6f5d06df4ca6287950dbc9c61550167
vaishnavi0305 <vaishnaviburadkar@gmail.com>
2025-07-02 14:57:17 +0530

    Condition and loops practice

---
+++ Condition_loops_02.py
@@ -0,0 +1,18 @@
+# If/else and loops
+
+age = 20
+if age >= 18:
+    print("You are eligible")
+else:
+    print("Not eligible")
+
+# Loops
+
+for i in range(3):
+    print("Loop count:", i)
+
+x = 1
+while x <= 3:
+    print("While Loop:", x)
+    x += 1
+
